8ecbf86fe099a652d85ab81a0fa2ba62a1312129
Pedro Marchante <pmarc560@gmail.com>
2019-08-28 15:59:22 -0400

    completed list

--- user-ui/src/features/activities/dashboard/ActivityDashboard.tsx
+++ user-ui/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Grid, GridColumn, List } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
 //make an interface so the component knows it is getting a prop of type IActivity
 interface IProps{
     activities:IActivity[]
@@ -10,13 +11,7 @@ const ActivityDashboard: React.FC<IProps> =({activities})=>{
     return(
     <Grid>
         <Grid.Column width={10}>
-        <List>
-          {activities.map(activityWegotBack => (
-            <List.Item key={activityWegotBack.id}>
-              {activityWegotBack.title}
-            </List.Item>
-          ))}
-        </List>
+          <ActivityList activities={activities}/>
         </Grid.Column>
     </Grid>
     );
--- user-ui/src/features/activities/dashboard/ActivityList.tsx
+++ user-ui/src/features/activities/dashboard/ActivityList.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { IActivity } from '../../../app/models/activity';
+import { List } from 'semantic-ui-react';
 interface IProp{
     activities:IActivity[]
 }
 
-const ActivityList: React.FC<IProp>=()=>{
+const ActivityList: React.FC<IProp>=({activities})=>{
 
-    return()
+    return(
+    <Fragment>
+        <List>
+                {activities.map(activityWegotBack => (
+            <List.Item key={activityWegotBack.id}>
+                {activityWegotBack.title}
+            </List.Item>
+            ))}
+        </List>
+      </Fragment>)
 }
 
 export default ActivityList;
